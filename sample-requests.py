e171ec5e8d912c529f0be0802483f1f853502782
Damir Imangulov <dimangulov@gmail.com>
2020-07-10 10:43:39 +0300

    initial commit

---
+++ sample-requests.py
@@ -0,0 +1,36 @@
+import requests
+
+def print_dict(label, obj):
+    print(label, " - ", obj)
+    for x in obj:
+        next_label = label + " - " + x
+        next_obj = obj[x]
+        if isinstance(next_obj, dict):
+            print_dict(next_label, next_obj)
+        else:
+            print(next_label, " - ", next_obj)
+
+
+XAPITOKEN = "OG3T44R1Wh1qOSbNvVJ2LR2IOYBuVXcoHlrSFCIr"
+headers = {"X-API-TOKEN": XAPITOKEN}
+
+baseUrl = "https://fra1.qualtrics.com/API/v3"
+surveyUrl = "/surveys"
+
+surveysResponse = requests.get(baseUrl+surveyUrl, headers=headers)
+surveys = surveysResponse.json()
+
+# print(surveys)
+
+for survey in surveys['result']['elements']:
+    print(survey['id'], "\t", survey['name'])
+
+# print("Choose one of them, print id")
+# survey_id = input()
+survey_id = surveys['result']['elements'][0]['id']
+surveyDefinitionUrl = "/survey-definitions/"+survey_id
+surveysDefinitionResponse = requests.get(baseUrl+surveyDefinitionUrl, headers=headers)
+surveyDefinition = surveysDefinitionResponse.json()
+
+print_dict("survey", surveyDefinition)
+
